a8c6343045c216962290c15d4b81835443cdee14
pavan_devops_eng <ec2-user@ip-172-31-15-185.ap-south-1.compute.internal>
2024-09-05 12:16:52 +0000

    m2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello Devops!");
-    System.out.println("Hello cloud!");
+    System.out.println("Hello aws_cloud!");
   }
 }
